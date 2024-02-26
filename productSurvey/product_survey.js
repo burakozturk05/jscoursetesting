1ce9aa2ccf34fe3714e5b38dc34d6c0f0da29991
burakozturk05 <burak.ozturkk05@gmail.com>
2024-02-26 18:17:14 +0300

    Practice completed2

---
+++ productSurvey/product_survey.js
@@ -0,0 +1,19 @@
+function submitFeedback(){
+    alert('Thank you for your valuable feedback')
+    document.getElementById('userName').innerHTML = username;
+    document.getElementById('userAge').innerHTML = age;
+    document.getElementById('userEmail').innerHTML = email;
+    document.getElementById('userJob').innerHTML = job;
+    document.getElementById('userDesignation').innerHTML = designation;
+    document.getElementById('userProductChoice').innerHTML = productType;
+    document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userInfo').style.display = 'block';
+}
+const submitButton=document.getElementById('submitBtn');
+submitButton.onclick = submitFeedback;
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      submitFeedback();
+    }
+  });
